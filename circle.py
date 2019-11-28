8aa429a06157b7d163ae4bb0e6ec56ad070f69f9
jln <julian.dangelmaier@gmail.com>
2019-11-28 01:27:24 +0100

    more cleanup

--- circle.py
+++
@@ -1,9 +0,0 @@
-import math
-x = 100;
-y= 100;
-rad = 11;
-
-for i in range(0,12):
-    print(str(i*30)+"   x:"+str(x+rad *math.cos(math.radians(i*30))) + "     y:"+str(y+rad*math.sin(math.radians(i*30))))
-
-
